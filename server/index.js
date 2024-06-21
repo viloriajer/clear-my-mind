7469eabc27346a19e6918ac792bd1edf2aa57477
Jeremy Viloria <viloria.jer@gmail.com>
2024-06-20 23:45:07 -0700

    cleanup unused code

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 import express from "express";
 import { query } from "./dbPool.js";
 import dotenv from "dotenv";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
 app.use(express.static("public"));
 
