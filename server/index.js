a8b1cc5bb9482333311b95ef443473146cc59de9
Jeremy Viloria <viloria.jer@gmail.com>
2024-06-20 22:34:09 -0700

    serve html page from root

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { query } from "./dbPool.js";
 import dotenv from "dotenv";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 
 dotenv.config();
@@ -12,6 +17,10 @@ app.get("/activity", async function (req, res) {
 		LIMIT 1;`
 	);
 	res.send(result);
+});
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "../index.html"));
 });
 app.listen(process.env.PORT, function () {
 	console.log(`clear-my-mind app listening on port ${process.env.PORT}!`);
